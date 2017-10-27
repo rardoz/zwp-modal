984c9bda8f58b09402240cc9caf5dadeb888fd59
Robert Greene <rgreene@yec.co>
2017-10-27 12:07:40 -0700

    Add ignore file

--- uninstall.php
+++ uninstall.php
@@ -6,7 +6,7 @@
  * When populating this file, consider the following flow
  * of control:
  *
- * - This method should be static
+ * - This method should be static 
  * - Check if the $_REQUEST content actually is the plugin name
  * - Run an admin referrer check to make sure it goes through authentication
  * - Verify the output of $_GET makes sense
--- zwp-modal.php
+++ zwp-modal.php
@@ -6,18 +6,18 @@
  * This file is read by WordPress to generate the plugin information in the plugin
  * admin area. This file also includes all of the dependencies used by the plugin,
  * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
+ * that starts the plugin...
  *
  * @link              http://zardozcs.com
  * @since             1.0.0
- * @package           Zwp_Modal
+ * @package           Zwp_Modal 
  *
  * @wordpress-plugin
  * Plugin Name:       ZWP Modal
  * Plugin URI:        https://github.com/rardoz/zwp-modal.git
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
- * Author:            Zardoz Creative Studio
+ * Author:            Zardoz Creative Studio 
  * Author URI:        http://zardozcs.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
