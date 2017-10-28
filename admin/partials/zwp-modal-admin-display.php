eb2d85bf64944eeb12c098a22aa8a8fc2bb7d885
Robert Greene <robert@zardozcs.com>
2017-10-28 12:25:37 -0700

    Make admin UI

--- admin/partials/zwp-modal-admin-display.php
+++ admin/partials/zwp-modal-admin-display.php
@@ -1,36 +1,21 @@
 <?php
-
-/**
- * Provide a admin area view for the plugin
- *
- * This file is used to markup the admin-facing aspects of the plugin.
- *
- * @link       http://zardozcs.com
- * @since      1.0.0
- *
- * @package    Zwp_Modal
- * @subpackage Zwp_Modal/admin/partials
- */
+  /**
+   * Provide a admin area view for the plugin
+   *
+   * This file is used to markup the admin-facing aspects of the plugin.
+   *
+   * @link       http://zardozcs.com
+   * @since      1.0.0
+   *
+   * @package    Zwp_Modal
+   * @subpackage Zwp_Modal/admin/partials
+   */
+  $pages = get_pages(); 
+  $posts = get_posts( ['posts_per_page' => 1000]); 
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
 <h2><?php esc_attr_e( 'ZWP Modal Settings', 'WpAdminStyle' ); ?></h2>
-
-<!-- select a page -->
-<select name="page-dropdown"> 
-  <option value="">
-    <?php echo esc_attr( __( 'Select page' ) ); ?>
-  </option> 
- <?php 
-  $pages = get_pages(); 
-  foreach ( $pages as $page ) {
-    $option = '<option value="' . get_page_link( $page->ID ) . '">';
-    $option .= $page->post_title;
-    $option .= '</option>';
-    echo $option;
-  }
- ?>
-</select>
 
 <!-- select a post to render in the modal
 
@@ -41,19 +26,30 @@ then we will save each key value pair in the db
 ie pageID => postID.
 
  -->
-<select name="post-dropdown"> 
-  <option value="">
-    <?php echo esc_attr( __( 'Select post to display' ) ); ?>
-  </option> 
- <?php 
-  $posts = get_posts( ['posts_per_page' => 1000]); 
-  foreach ( $posts as $post ) {
-    $option = '<option value="' . get_page_link( $post->ID ) . '">';
-    $option .= $post->post_title;
-    $option .= '</option>';
-    echo $option;
-  }
- ?>
-</select>
 
 
+ <form method="post" name="cleanup_options" action="options.php">
+    <?php foreach($pages as $page): ?>
+      <fieldset>
+        <legend class="screen-reader-text"><span>Clean WordPress head section</span></legend>
+        <label for="<?= $this->plugin_name ?>-<?= $page->ID ?>">
+          <a href="<?= get_page_link( $page->ID ) ?>" target="_blank"><?= $page->post_title ?></a>
+        </label>
+        <br />
+        <select
+          id="<?= $this->plugin_name ?>-<?= $page->ID ?>"
+          name="<?= $this->plugin_name ?>[pages][<?= $page->ID ?>]"
+        >
+          <option value="">Select post</option>
+          <?php foreach($posts as $post): ?>
+              <option value="<?= $post->ID ?>">
+                <?= $post->post_title ?>
+              </option>
+          <?php endforeach?>
+        </select>
+      </fieldset>
+      <hr />
+    <?php endforeach ?>
+
+    <?php submit_button('Save all changes', 'primary','submit', TRUE); ?>
+</form>
